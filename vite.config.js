1a7abf3666820af270f821f59070f3acdfec56b2
kridesav <savolainenkristjan@gmail.com>
2023-11-20 19:00:08 +0200

    gh pages fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ticketguru-client/',
+  base: '/TicketGuru_client/',
   plugins: [react()],
 })
